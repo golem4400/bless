c0a10d7e3654d61918e90aebe6a40bcb198ac1e1
golem4400 <88250121+golem4400@users.noreply.github.com>
2024-11-18 08:09:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzM5Nzg3YTg2ZjMwNzQ4NGY2YmI5OTMiLCJwdWJsaWNBZGRyZXNzIjoiN3d4Nlc1dEc3OE5YdEZEdGI2WDZLaHBtSHh2anI1NGNWY0JaeVhHeU4xQngiLCJ3YWxsZXRUeXBlIjoic29sYW5hIiwiaWF0IjoxNzMxODE5NjQzLCJleHAiOjE3NjMzNzcyNDN9.PCM1YdSlLRz7yPWNrq2X2gR8EZ3M0UY3MmMv4SWDyHs',
+        nodes: [
+            { nodeId: '12D3KooWAwmmCRQnoBhUMKsHuLyDu4L72jeUPS8v9qsZSbRgtxcb', hardwareId: 'd1c674a9c6f976192d1398023b32676e3a659afaabce8862cb5aadf0fecf7f11', proxy: '' }
+        ]
+    }
+    // Add more usertokens as needed
+];
